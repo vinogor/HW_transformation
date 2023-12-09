6ce933d85946e0e13107a80afc1b8294d4af5307
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2023-12-09 23:59:53 +0300

    fix3

--- Assets/MyStuff/Scripts/HW-EnemyGeneration/Enemy.cs
+++ Assets/MyStuff/Scripts/HW-EnemyGeneration/Enemy.cs
@@ -5,13 +5,13 @@ public class Enemy : MonoBehaviour
     private float _speed = 2f;
     private Vector3 _vector;
 
-    private void Update()
-    {
-        transform.Translate(_vector * (_speed * Time.deltaTime));
-    }
-
     public void SetMotionVector(Vector3 vector)
     {
         _vector = vector;
+    }
+
+    private void Update()
+    {
+        transform.Translate(_vector * (_speed * Time.deltaTime));
     }
 }
