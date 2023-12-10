df6240fd9c687c8c63d1b7109f9eefc226d255cd
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2023-12-10 23:56:42 +0300

    ref

--- Assets/MyStuff/Scripts/HW-AntiFraudAlarm/Alarm.cs
+++ Assets/MyStuff/Scripts/HW-AntiFraudAlarm/Alarm.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class Alarm : MonoBehaviour
 {
+    private const int MaxVolume = 1;
+    private const int MinVolume = 0;
+
+    [SerializeField] private float _volumeChangeSpeed = 0.5f;
+
     private AudioSource _audioSource;
-    private float _volumeChangeSpeed = 0.5f;
     private bool _isSoundOn;
 
     private void Start()
@@ -16,13 +20,15 @@ public class Alarm : MonoBehaviour
     {
         if (_isSoundOn)
         {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, 1, _volumeChangeSpeed * Time.deltaTime);
+            _audioSource.volume =
+                Mathf.MoveTowards(_audioSource.volume, MaxVolume, _volumeChangeSpeed * Time.deltaTime);
         }
         else
         {
-            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, 0, _volumeChangeSpeed * Time.deltaTime);
+            _audioSource.volume =
+                Mathf.MoveTowards(_audioSource.volume, MinVolume, _volumeChangeSpeed * Time.deltaTime);
 
-            if (_audioSource.volume == 0)
+            if (_audioSource.volume == MinVolume)
             {
                 _audioSource.Stop();
             }
