8b9dc0e7479034cf29002f6a39979387d1fe40f5
Aleksandr Andreev <andreev.aleksandr.spb@gmail.com>
2023-12-11 15:55:42 +0300

    add event

--- Assets/MyStuff/Scripts/HW-AntiFraudAlarm/Alarm.cs
+++ Assets/MyStuff/Scripts/HW-AntiFraudAlarm/Alarm.cs
@@ -1,51 +1,20 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Alarm : MonoBehaviour
 {
-    private const int MaxVolume = 1;
-    private const int MinVolume = 0;
-
-    [SerializeField] private float _volumeChangeSpeed = 0.5f;
-
-    private AudioSource _audioSource;
-    private bool _isSoundOn;
-
-    private void Start()
-    {
-        _audioSource = GetComponentInChildren<AudioSource>();
-        _audioSource.volume = 0;
-    }
-
-    private void Update()
-    {
-        if (_isSoundOn)
-        {
-            ChangeVolumeTo(MaxVolume);
-        }
-        else
-        {
-            ChangeVolumeTo(MinVolume);
-
-            if (_audioSource.volume == MinVolume)
-            {
-                _audioSource.Stop();
-            }
-        }
-    }
+    [SerializeField] private UnityEvent thiefEntered;
+    [SerializeField] private UnityEvent thiefRunAway;
 
     private void OnTriggerEnter(Collider other)
     {
-        _isSoundOn = true;
-        _audioSource.Play();
+        Debug.Log("OnTriggerEnter");
+        thiefEntered?.Invoke();
     }
 
     private void OnTriggerExit(Collider other)
     {
-        _isSoundOn = false;
-    }
-
-    private void ChangeVolumeTo(int targetVolume)
-    {
-        _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, _volumeChangeSpeed * Time.deltaTime);
+        Debug.Log("OnTriggerExit");
+        thiefRunAway?.Invoke();
     }
 }
